a766d6097711bc354135fdaa2790761fc30060a8
Kaushal Kumar Agarwal <kaushalag29@gmail.com>
2018-08-14 22:01:41 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -2,4 +2,6 @@ import os
 os.system("pip install mechanize")
 os.system("pip install pycrypto")
 os.system("pip install BeautifulSoup")
+os.system("pip install nmap")
+os.system("pip install bs4")
 
